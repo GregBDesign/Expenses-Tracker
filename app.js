44bafb563e46fc4c85c88ea10592fbc9ed13ad7b
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-03-26 15:59:15 +1100

    Updates to function and styling

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('form');
 const expenseList = document.querySelector('#expensesList');
-const runnningTotal = document.querySelector('#runVal');
 
 //add expense item on submit
 form.addEventListener('submit', function(e){
@@ -17,16 +16,16 @@ addExpense = () => {
         newTd.textContent = form[i].value;
         newTr.append(newTd);
     }
-    deleteBtn.textContent = "X";
+    deleteBtn.classList.add("delete");
     newTr.append(deleteBtn);
     expenseList.append(newTr);
 }
 
 //delete button functionality
 expenseList.addEventListener('click', function(e){
-    if(e.target.textContent === 'X'){
         e.target.parentNode.remove();
-    }
 })
 
-//to do: running total & styling
+
+
+//to do: styling
